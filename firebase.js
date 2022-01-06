f48be2506a3c0a84f7d71192110e24ea82c58c75
Umar <umar.kt.24129@gmail.com>
2022-01-06 19:53:14 +0100

    slight_changes

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ var serviceAccount = require("./firebasekey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://login-18f06.firebaseio.com"
+  // databaseURL: "https://login-18f06.firebaseio.com"     // Add your own firebase login
 });
 
 ///////
